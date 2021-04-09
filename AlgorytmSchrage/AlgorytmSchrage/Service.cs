2ac0ebdad0e2b4b14567b7c3d8a66d93f3b454f0
veloce\mateusz.bartkowiak <mateusz.bartkowiak@veloce.local>
2021-04-09 20:56:01 +0200

    add algorythm Schrage

---
+++ AlgorytmSchrage/AlgorytmSchrage/Service.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorytmSchrage
+{
+    public static class Service
+    {
+        
+    }
+}
---
+++ AlgorytmSchrage/AlgorytmSchrage/TaskLine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorytmSchrage
+{
+    public class TaskLine
+    {
+        public int P { get; set; }
+        public int Q { get; set; }
+        public int R { get; set; }
+
+        public TaskLine(string r, string p,string q)
+        {
+            P = Convert.ToInt32(p);
+            Q = Convert.ToInt32(q);
+            R = Convert.ToInt32(r);
+        }
+
+        public TaskLine(int r, int p, int q)
+        {
+            P = p;
+            Q = q;
+            R = r;
+        }
+    }
+}
