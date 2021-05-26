5a9491dcd4e8ed3ec32cda369e80ff6172d9143e
veloce\mateusz.bartkowiak <mateusz.bartkowiak@veloce.local>
2021-05-26 20:09:23 +0200

    Neh

---
+++ NehAlgoritmo/NehAlgoritmo/Neh.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NehAlgoritmo
+{
+    public static class Neh
+    {
+        public static int NehAlgorithm(List<List<int>> list)
+        {
+            List<List<int>> copy = new List<List<int>>(list);
+            List<List<int>> pi = new List<List<int>>();
+            List<int> job = new List<int>();
+
+            int cmax = int.MaxValue;
+            int n = copy.Count();
+
+            for (int i = 0; i < copy[n - 1].Count(); i++)
+            {
+                job.Add(copy[n - 1][i]);
+            }
+            copy.RemoveAt(copy.Count() - 1);
+            pi.Add(job);
+            job.Clear();
+
+            n = copy.Count();
+
+            for (int i = n - 1 ; i > 0; i--)
+            {
+                for (int j = 0; j < copy[i].Count(); j++)
+                {
+                    job.Add(copy[i][j]);
+                }
+                copy.RemoveAt(copy.Count() - 1);
+
+                List<List<int>> result = new List<List<int>>();
+                cmax = FindBestTaskPosition(ref pi, job);
+                job.Clear();
+            }
+
+            return cmax;
+        }
+
+        public static int FindBestTaskPosition(ref List<List<int>> list, List<int> task)
+        {
+            List<List<int>> newPi = new List<List<int>>();
+            int cmax = int.MaxValue;
+
+            for(int i = 0; i <= list.Count(); i++)
+            {
+                List<List<int>> copy = new List<List<int>>(list);
+
+                copy.Insert(i, task);
+
+                int c = CalculateCmax(copy);
+
+                if(c < cmax)
+                {
+                    cmax = c;
+                    newPi = copy;
+                }
+            }
+
+            list = newPi;
+
+            return cmax;
+        }
+
+        public static int CalculateCmax(List<List<int>> list)
+        {
+            int n = list.Count();
+            int m = list[0].Count();
+            List<List<int>> endTimes = new List<List<int>>();
+
+            endTimes.Clear();
+
+            for(int i = 0; i < n; i++)
+            {
+                List<int> tmp = new List<int>();
+                endTimes.Add(tmp);
+
+                for(int j = 0; j < m; j++)
+                {
+                    endTimes[i].Add(0);
+                }
+            }
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                {
+                    if(i == 0 && j == i)
+                    {
+                        endTimes[i][j] = list[i][j];
+                    }
+                    else if(i == 0)
+                    {
+                        endTimes[i][j] = list[i][j] + endTimes[i][j - 1];
+                    }
+                    else if(j == 0)
+                    {
+                        endTimes[i][j] = list[i][j] + endTimes[i - 1][j];
+                    }
+                    else
+                    {
+                        if(endTimes[i][j-1] > endTimes[i - 1][j])
+                        {
+                            endTimes[i][j] = list[i][j] + endTimes[i][j - 1];
+                        }
+                        else
+                        {
+                            endTimes[i][j] = list[i][j] + endTimes[i - i][j];
+                        }
+                    }
+                }
+            }
+            return endTimes[n - 1][m - 1];
+        }
+    }
+}
---
+++ NehAlgoritmo/NehAlgoritmo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NehAlgoritmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Podaj nazwe pliku z pulpitu");
+            string nameOfFIle = Console.ReadLine();
+            string[] lines = System.IO.File.ReadAllLines(@"D:\Users\User\Desktop\" + nameOfFIle + ".DAT");
+            string[] tmp = lines[0].Split(' ');
+            int n = Convert.ToInt32(tmp[0]);
+            int m = Convert.ToInt32(tmp[1]);
+            List<List<int>> TaskList = new List<List<int>>();
+            List<int> SumOfTimes = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                TaskList.Add(new List<int>());
+                tmp = lines[i + 1].Split(' ');
+                int sum = 0;
+
+                for (int j = 0; j < m; j++)
+                {
+                    sum += Convert.ToInt32(tmp[j]);
+                    TaskList[i].Add(Convert.ToInt32(tmp[j]));
+                }
+                SumOfTimes.Add(sum);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j)
+                    {
+                        if (SumOfTimes[i] < SumOfTimes[j])
+                        {
+                            var tmpSwap = SumOfTimes[i];
+                            SumOfTimes[i] = SumOfTimes[j];
+                            SumOfTimes[j] = tmpSwap;
+
+                            var tmpSwap2 = TaskList[i];
+                            TaskList[i] = TaskList[j];
+                            TaskList[j] = tmpSwap2;
+                        }
+                    }
+                }
+            }
+
+            int result = Neh.NehAlgorithm(TaskList);
+            Console.WriteLine($"Cmax = {result}");
+        }
+    }
+}
